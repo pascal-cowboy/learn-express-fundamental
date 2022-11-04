735bac3dfb6c72cf6f0de9b29c851150b5a5f901
alvittoag <alvittogenz09@gmail.conm>
2022-11-04 14:12:11 +0700

    edit

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-
-app.use(logger);
-app.get("/", logger, (req, res) => {
-  res.send("home");
-});
-app.get("/about", logger, (req, res) => {
-  res.send("about");
-});
-
-app.get("/products", logger, (req, res) => {
-  res.send("products");
-});
 
 app.listen(5000, () => {
   console.log("Server is Listening on port 5000....");
