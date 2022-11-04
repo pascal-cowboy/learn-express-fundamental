0509cd7ab16b9bb3a1206a31f5be52b373aeb45f
alvittoag <alvittogenz09@gmail.conm>
2022-11-04 12:45:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const logger = require("./logger");
+//! req => middleware = res
+
+app.use(logger);
+
+app.get("/", logger, (req, res) => {
+  res.send("home");
+});
+app.get("/about", logger, (req, res) => {
+  res.send("about");
+});
+app.get("/products", logger, (req, res) => {
+  res.send("products");
+});
+
+app.listen(5000, () => {
+  console.log("Server is Listening on port 5000....");
+});
