df32d9742225b8546b1e510876b868ee2bd69799
alvittoag <alvittogenz09@gmail.conm>
2022-11-04 12:51:07 +0700

    edit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const logger = require("./logger");
-//! req => middleware = res
 
 app.use(logger);
 app.get("/", logger, (req, res) => {
